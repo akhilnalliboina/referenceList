4c3d986a20583fdcf701bbd3caa1c2299b923432
akhiln95 <akhilnalliboina@gmail.com>
2020-07-07 21:12:52 +0200

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+class Search extends Component {
+  state = {
+    searchValue: "",
+    meals: []
+    };
+    handleOnChange = event => {
+      this.setState({ searchValue: event.target.value });
+      };
+      handleSearch = () => {
+        this.makeApiCall(this.state.searchValue);
+        };
+
+       makeApiCall = searchInput => {
+       fetch(`https://api.crossref.org/works/${searchInput}`).then(data=>data.json())
+       .then(dataSD=>this.setState({ meals:dataSD.message.reference}))
+        };
+        ;
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Welcome to the references (Backward search) using DOI</h1>
+    <input
+name="text"
+type="text"
+placeholder="Search using DOI"
+onChange={event => this.handleOnChange(event)}
+value={this.state.searchValue}
+/>
+<button onClick={this.handleSearch}>Search</button>
+<p></p>  
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>DOI</th>
+      <th>Year</th>
+    </tr>
+  </thead>
+  <tbody>
+        {
+               this.state.meals ? (this.state.meals.map((dynamicData) =>
+                {return(
+                  <tr>
+                <td>{dynamicData.unstructured}</td>
+                <td>{dynamicData.DOI}</td>
+                <td>{dynamicData.year}</td>
+                </tr>
+                )
+                })):<tr><td colSpan="5">Loading...</td></tr> }
+                 
+                </tbody>
+                </Table> 
+              
     </div>
-  );
+    
+    );
 }
-
-export default App;
+}
+export default Search;
