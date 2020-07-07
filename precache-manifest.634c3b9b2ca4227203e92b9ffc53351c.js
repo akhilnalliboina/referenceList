7ea44d459db9f064df44574d4ac0d6f515690fa2
akhiln95 <akhilnalliboina@gmail.com>
2020-07-07 21:37:11 +0200

    Updates

---
+++ precache-manifest.634c3b9b2ca4227203e92b9ffc53351c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c60f6199146ceea386065b95c371ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "192b1065a5a497cad142",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3a1ac17a866932a10b5e",
+    "url": "/static/css/main.728e476a.chunk.css"
+  },
+  {
+    "revision": "192b1065a5a497cad142",
+    "url": "/static/js/2.59380d86.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.59380d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1ac17a866932a10b5e",
+    "url": "/static/js/main.94d52bdc.chunk.js"
+  },
+  {
+    "revision": "9438b99032869c455f54",
+    "url": "/static/js/runtime-main.aca3bc04.js"
+  }
+]);
